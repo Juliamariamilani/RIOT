356c90394315f6a4d166b3bd8c8df6e1b363be20
Bas Stottelaar <basstottelaar@gmail.com>
2018-05-29 20:48:07 +0200

    drivers: tpuart: add initial support

--- sys/net/gnrc/netif/init_devs/init.c
+++ sys/net/gnrc/netif/init_devs/init.c
@@ -182,4 +182,9 @@ void gnrc_netif_init_devs(void)
         auto_init_ncn5120();
     }
 
+    if (IS_USED(MODULE_TPUART)) {
+        extern void auto_init_tpuart(void);
+        auto_init_tpuart();
+    }
+
 }
